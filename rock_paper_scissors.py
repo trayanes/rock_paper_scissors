3f4be833b0e74eec6e75997bfd17a04ff8044e6b
Trayan Chernev <trayanes@yahoo.com>
2023-05-14 15:42:24 +0300

    update
    update

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,5 @@
 import random
+from colorama import Fore, Back, Style
 
 option_list = ["Rock", "Paper", "Scissors"]
 computer_move = random.choice(option_list)
@@ -31,16 +32,27 @@ while True:
             invalid_input = True
             continue
 
+win_counter = 0
+lost_counter = 0
+draw_counter = 0
+
 if player_move.lower() == "rock" and computer_move.lower() == "scissors" or \
         player_move.lower() == "paper" and computer_move.lower() == "rock" or \
         player_move.lower() == "scissors" and computer_move.lower() == "paper":
-    print(f"The Computer chose {computer_move}.")
-    print("You win")
+    win_counter += 1
+    print(Fore.CYAN+f"The Computer chose {computer_move}.")
+    print(Fore.YELLOW + "You win")
+
 elif computer_move.lower() == "rock" and player_move.lower() == "scissors" or \
         computer_move.lower() == "paper" and player_move.lower() == "rock" or \
         computer_move.lower() == "scissors" and player_move.lower() == "paper":
-    print(f"The Computer chose {computer_move}.")
-    print("You lose.Better luck next time")
+    lost_counter += 1
+    print(Fore.CYAN+f"The Computer chose {computer_move}.")
+    print(Fore.RED + "You lose.Better luck next time.")
+
 else:
-    print(f"The Computer chose {computer_move}")
-    print("It is a draw.")
+    draw_counter += 1
+    print(Fore.CYAN + f"The Computer chose {computer_move}")
+    print(Fore.GREEN + "It is a draw.")
+
+print("Would you like to play more??")
---
+++ test.py
@@ -0,0 +1,2 @@
+from colorama import Fore, Back, Style
+print(Fore.RED, Style.BRIGHT + "green text")
